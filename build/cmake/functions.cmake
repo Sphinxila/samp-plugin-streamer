3cc35bda698844bcdfb3ca20a00d55493b830743
Sphinx <sphinx@sphinx.li>
2017-02-23 01:07:33 +0100

    make includes private

--- build/cmake/functions.cmake
+++ build/cmake/functions.cmake
@@ -6,7 +6,7 @@ function(streamer_add_samp_plugin name)
 	target_link_libraries(${name} sampgdk sampsdk)
 
 	# Directories
-	target_include_directories(streamer PUBLIC ${SAMP_STREAMER_EXTERNAL})			# Include externals
+	target_include_directories(streamer PRIVATE ${SAMP_STREAMER_EXTERNAL})			# Include externals
 	target_include_directories(streamer PRIVATE ${SAMPGDK_DIR}/include)				# Include sampgdk files
 	target_include_directories(streamer PRIVATE ${SAMPGDK_GEN_DIR}/include/sampgdk)	# Include sampgdk generated samp includes
 
@@ -22,7 +22,7 @@ function(streamer_add_library name)
 	target_link_libraries(${name} sampgdk sampsdk)
 
 	# Directories
-	target_include_directories(${name} PUBLIC ${SAMP_STREAMER_EXTERNAL})			# Include externals
+	target_include_directories(${name} PRIVATE ${SAMP_STREAMER_EXTERNAL})			# Include externals
 	target_include_directories(${name} PRIVATE ${SAMPGDK_DIR}/include)				# Include sampgdk files
 	target_include_directories(${name} PRIVATE ${SAMPGDK_GEN_DIR}/include/sampgdk)	# Include sampgdk generated samp includes
 
